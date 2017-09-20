9c71887e61a0d81f22980239a6e8afc04a1a28b0
nilsanderselde <nils.elde@gmail.com>
2017-09-20 06:02:01 -0400

    changed windows keyboard layout filename from ingg-fun to ingfun so that the abbreviation would better reflect syllabic boundaries

--- levdist/main.go
+++ levdist/main.go
@@ -1,23 +1,17 @@
 // Nils Elde
 // https://gitlab.com/nilsanderselde
 //
-// editDistance and editDistStep converted to Go from Python, based on:
-//
-// Natural Language Toolkit: Distance Metrics
-//
-// Copyright (C) 2001-2017 NLTK Project
-// Author: Edward Loper <edloper@gmail.com>
-//         Steven Bird <stevenbird1@gmail.com>
-//         Tom Lippincott <tom@cs.columbia.edu>
-// URL: <http://nltk.org/>
-//
+// Calculates Levenshtein distances between words stored in a tabular text file.
+// Includes commented out print statements which can help demonstrate how this
+// algorithm works
+
 package levdist
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -36,93 +30,90 @@ func GetDistances() [][]string {
 	for scanner.Scan() {
 		current := scanner.Text()
 		words := strings.Split(current, "\t")
-		distance := EditDistance(words[0], words[1], 1, false)
-		results = append(results, []string{words[0], words[1], fmt.Sprintf("%d", distance)})
-
+		distance := FindDistance([]rune(words[0]), []rune(words[1]), true)
+		results = append(results, []string{words[0], words[1], strconv.Itoa(distance)})
 	}
-
 	return results
 }
 
-// EditDistance calculates Levenshtein distance between two words
-func EditDistance(s1 string, s2 string, substitutionCost int, transpositions bool) int {
-	// convert to rune array to access each unicode letter
-	r1 := []rune(s1)
-	r2 := []rune(s2)
+// FindDistance calculates Levenshtein distance between two words
+// If flipping is true, flipping two adjacent letters counts as one move
+func FindDistance(word1 []rune, word2 []rune, flipping bool) int {
 
-	// set up a 2-D array
-	var len1 = len(r1)
-	var len2 = len(r2)
-	var lev = make([][]int, len1+1)
-	for i := 0; i < len1+1; i++ {
-		lev[i] = make([]int, len2+1)
+	// Get length of each word
+	var length1 = len(word1)
+	var length2 = len(word2)
+	// fmt.Printf("\nlen(%s) = %d\n", string(word1), length1)
+	// fmt.Printf("len(%s) = %d\n", string(word2), length2)
+
+	// Create a 2D array whose dimensions are the length of each word + 1
+	// fmt.Println("initial array: ")
+	var pathArray = make([][]int, length1+1)
+	for i := 0; i < length1+1; i++ {
+		pathArray[i] = make([]int, length2+1)
 	}
-
-	for i := 0; i < len1; i++ {
-		lev[i][0] = i // column 0: 0,1,2,3,4,...
+	for i := 0; i < length1+1; i++ {
+		pathArray[i][0] = i // column 0: 0,1,2,3,4,...
 	}
-
-	for j := 0; j < len2; j++ {
-		lev[0][j] = j // row 0: 0,1,2,3,4,...
+	for j := 0; j < length2+1; j++ {
+		pathArray[0][j] = j // row 0: 0,1,2,3,4,...
 	}
+	// for _, element := range pathArray {
+	// 	fmt.Println(element)
+	// }
 
-	// iterate over the array
-	for i := 0; i < len1; i++ {
-		for j := 0; j < len2; j++ {
-			editDistStep(lev, i+1, j+1, r1, r2, substitutionCost, transpositions)
+	// Compare each letter in first word with each letter in second word
+	for i := 0; i < length1; i++ {
+		for j := 0; j < length2; j++ {
+			rune1 := word1[i]
+			rune2 := word2[j]
 
-			// print out progress in console to illustrate how it works
-			// fmt.Print(lev[i][j])
-			// if i < len1-1 {
-			// 	fmt.Print("\n")
-			// }
+			// Deleting a letter
+			del := pathArray[i][j+1] + 1
+			// fmt.Print("del = " + strconv.Itoa(del))
+
+			// Inserting a letter
+			ins := pathArray[i+1][j] + 1
+			// fmt.Print("; ins = " + strconv.Itoa(ins))
+
+			// Replacing a letter
+			rep := pathArray[i][j]
+			if rune1 != rune2 {
+				rep++
+			}
+			// fmt.Print("; rep = " + strconv.Itoa(rep))
+
+			// Flipping letters (ab -> ba) (if enabled)
+			flp := rep + 1
+			if flipping && i+1 > 1 && j+1 > 1 {
+				if word1[i-1] == rune2 && word2[j-1] == rune1 {
+					flp = pathArray[i-1][j-1] + 1
+				}
+			}
+			// fmt.Print("; flp = " + strconv.Itoa(flp))
+
+			// Set current array value to whichever path is shortest
+			min1 := del
+			if ins < del {
+				min1 = ins
+			}
+			min2 := rep
+			if flp < rep {
+				min2 = flp
+			}
+			if min1 < min2 {
+				pathArray[i+1][j+1] = min1
+				// fmt.Println("; min = " + strconv.Itoa(min1))
+			} else {
+				pathArray[i+1][j+1] = min2
+				// fmt.Println("; min = " + strconv.Itoa(min2))
+			}
 		}
 	}
-	return lev[len1][len2]
-}
+	// fmt.Println("final array: ")
+	// for _, element := range pathArray {
+	// 	fmt.Println(element)
+	// }
 
-func editDistStep(lev [][]int, i int, j int, r1 []rune, r2 []rune, substitutionCost int, transpositions bool) {
-	c1 := r1[i-1]
-	c2 := r2[j-1]
-
-	// skipping a character in s1
-	a := lev[i-1][j] + 1
-	// skipping a character in s2
-	b := lev[i][j-1] + 1
-
-	// substitution
-	c := lev[i-1][j-1]
-
-	if c1 != c2 {
-		c += substitutionCost
-	}
-
-	// transposition
-	d := c + 1 // never picked by default
-	if transpositions && i > 1 && j > 1 {
-		if r1[i-2] == c2 && r2[j-2] == c1 {
-			d = lev[i-2][j-2] + 1
-		}
-	}
-
-	// pick the cheapest
-
-	min1 := a
-	if b < a {
-		min1 = b
-	}
-
-	min2 := c
-	if d < c {
-		min2 = d
-	}
-
-	if min1 < min2 {
-		lev[i][j] = min1
-	} else {
-		lev[i][j] = min2
-	}
-
-	// lev[i][j] = int(math.Min(math.Min(float64(a), float64(b)), math.Min(float64(c), float64(d))))
-
+	return pathArray[length1][length2]
 }
