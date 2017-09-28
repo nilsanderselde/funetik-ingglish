cf2b4a9b0f76f9952fda145dc7cacab7db65f5d5
nilsanderselde <nils.elde@gmail.com>
2017-09-27 21:10:40 -0400

    db retrieving results, but not putting them into template

--- main.go
+++ main.go
@@ -5,73 +5,76 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strconv"
 
-	"gitlab.com/nilsanderselde/funetik-ingglish/customsort"
+	"gitlab.com/nilsanderselde/funetik-ingglish/dbconnect"
 	"gitlab.com/nilsanderselde/funetik-ingglish/levdist"
 	"gitlab.com/nilsanderselde/funetik-ingglish/params"
 	"gitlab.com/nilsanderselde/funetik-ingglish/runestats"
 )
 
 type templateHandler struct {
-	// once      sync.Once
-	filename  string
-	templ     *template.Template
-	path      string
-	args      params.Params
-	sortBy    string // template filename fragment
-	sortOrder string // template filename fragment
+	filename string
+	templ    *template.Template
+	path     string
+	query    string
+	start    int
+	num      int
+	args     params.Params
+	sortBy   string // template filename fragment
 }
 
 // handle http request
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	// t.once.Do(func() {
+	var err error
 	funcMap := template.FuncMap{
-		"GetStats":       runestats.GetStats,
-		"GetDistances":   levdist.GetDistances,
-		"SortWords":      customsort.SortWords,
-		"SortByTrud":     customsort.SortByTrud,
-		"SortByDistance": customsort.SortByDistance,
+		"GetStats":     runestats.GetStats,
+		"GetDistances": levdist.GetDistances,
+		"ShowWords":    dbconnect.PostgresIO,
 	}
 
 	if t.path == "/" {
 		t.sortBy = "words_sort_fun.html"
 		t.args.Fun = true
-		t.args.Altfun = false
 		t.args.Trud = false
 		t.args.Dist = false
-		t.args.Order = []rune("aäeiywuøolrmnbpvfgkdtzsžšh")
-		t.args.Reverse = false
 
 		if r.URL.Query()["sortby"] != nil {
-			if r.URL.Query()["sortby"][0] == "altfun" {
+			if r.URL.Query()["sortby"][0] == "trud" {
 				t.args.Fun = false
-				t.args.Altfun = true
-				// t.args.Trud = false
-				// t.args.Dist = false
-				t.args.Order = []rune("aäbdefghiklmnoøprsštuvwyzž") // sudo-trudišinul ordør
-			} else if r.URL.Query()["sortby"][0] == "trud" {
-				t.args.Fun = false
-				// t.args.Altfun = false
 				t.args.Trud = true
-				// t.args.Dist = false
 				t.sortBy = "words_sort_trud.html"
 			} else if r.URL.Query()["sortby"][0] == "dist" {
 				t.args.Fun = false
-				// t.args.Altfun = false
-				// t.args.Trud = false
 				t.args.Dist = true
 				t.sortBy = "words_sort_dist.html"
 			}
 		}
 		if r.URL.Query()["order"] != nil {
 			if r.URL.Query()["order"][0] == "desc" {
-				t.args.Reverse = true
-			} else {
-				// t.args.Reverse = false
-				// fmt.Println("test")
+				t.query += "ORDER BY subcipher DESC;"
 			}
+		} else {
+			t.query += ";"
 		}
+		if r.URL.Query()["start"] != nil {
+			t.start, err = strconv.Atoi(r.URL.Query()["start"][0])
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			t.start = 0
+		}
+		if r.URL.Query()["num"] != nil {
+			t.num, err = strconv.Atoi(r.URL.Query()["num"][0])
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			t.num = 10
+		}
+
 		t.templ = template.Must(template.New(t.filename).Funcs(funcMap).ParseFiles(
 			filepath.Join("templates", t.filename),
 			filepath.Join("templates", t.sortBy),
@@ -85,9 +88,8 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			filepath.Join("templates", "_footer.html"),
 		))
 	}
-	// })
-	// do once prevents dynamic rendering of templates based on query string
-	t.templ.Execute(w, t.args)
+
+	t.templ.Execute(w, dbconnect.PostgresIO(t.query, t.start, t.num))
 }
 
 // Sets HTTP headers for handler passed to function
@@ -102,7 +104,7 @@ func setHeaders(h http.Handler) http.Handler {
 
 func main() {
 	http.Handle("/static/", setHeaders(http.StripPrefix("/static/", http.FileServer(http.Dir("static")))))
-	http.Handle("/", setHeaders(&templateHandler{filename: "words.html", path: "/"}))
+	http.Handle("/", setHeaders(&templateHandler{filename: "words.html", path: "/", query: "SELECT * FROM formatted"}))
 	http.Handle("/runestats", setHeaders(&templateHandler{filename: "runestats.html"}))
 	http.Handle("/levdist", setHeaders(&templateHandler{filename: "levdist.html"}))
 
--- params/main.go
+++ params/main.go
@@ -3,9 +3,7 @@ package params
 // Params encapsulates data to be passed to mapped functions
 type Params struct {
 	Fun     bool
-	Altfun  bool
 	Trud    bool
 	Dist    bool
-	Order   []rune
 	Reverse bool
 }
