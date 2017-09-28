f24feb14b1a0b762e153dbbb7625489b3846c023
nilsanderselde <nils.elde@gmail.com>
2017-09-28 02:24:08 -0400

    working

--- main.go
+++ main.go
@@ -7,72 +7,132 @@ import (
 	"path/filepath"
 	"strconv"
 
-	"gitlab.com/nilsanderselde/funetik-ingglish/dbconnect"
 	"gitlab.com/nilsanderselde/funetik-ingglish/levdist"
 	"gitlab.com/nilsanderselde/funetik-ingglish/params"
 	"gitlab.com/nilsanderselde/funetik-ingglish/runestats"
+	"gitlab.com/nilsanderselde/funetik-ingglish/words"
 )
 
 type templateHandler struct {
-	filename string
-	templ    *template.Template
-	path     string
-	query    string
-	start    int
-	num      int
-	args     params.Params
-	sortBy   string // template filename fragment
+	filename  string
+	templ     *template.Template
+	path      string
+	query     string
+	queryFrom string
+	args      params.Params
+	sortBy    string // template filename fragment
 }
+
+const (
+	// DefaultNum is default number of words per page
+	DefaultNum int = 50
+)
 
 // handle http request
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	var err error
+	t.args.Query = t.query + t.queryFrom
+
 	funcMap := template.FuncMap{
 		"GetStats":     runestats.GetStats,
 		"GetDistances": levdist.GetDistances,
-		"ShowWords":    dbconnect.PostgresIO,
+		"ShowWords":    words.GetWords,
 	}
 
 	if t.path == "/" {
-		t.sortBy = "words_sort_fun.html"
-		t.args.Fun = true
-		t.args.Trud = false
+		t.sortBy = "words_sort_new.html"
+		t.args.New = true
+		t.args.Old = false
 		t.args.Dist = false
+		t.args.Reverse = false
+		t.args.CurrentPage = "?sortby=new"
 
 		if r.URL.Query()["sortby"] != nil {
-			if r.URL.Query()["sortby"][0] == "trud" {
-				t.args.Fun = false
-				t.args.Trud = true
-				t.sortBy = "words_sort_trud.html"
+			if r.URL.Query()["sortby"][0] == "old" {
+				t.args.New = false
+				t.args.Old = true
+				t.sortBy = "words_sort_old.html"
+				t.args.CurrentPage = "?sortby=old"
+
+				if r.URL.Query()["order"] != nil {
+					if r.URL.Query()["order"][0] == "desc" {
+						t.args.Query += " ORDER BY old DESC;"
+						t.args.Reverse = true
+					} else {
+						t.args.Query += " ORDER BY old ASC;"
+					}
+				} else {
+					t.args.Query += " ORDER BY old ASC;"
+				}
+
 			} else if r.URL.Query()["sortby"][0] == "dist" {
-				t.args.Fun = false
+				t.args.New = false
 				t.args.Dist = true
 				t.sortBy = "words_sort_dist.html"
+				t.args.CurrentPage = "?sortby=dist"
+
+				if r.URL.Query()["order"] != nil {
+					if r.URL.Query()["order"][0] == "desc" {
+						t.args.Query += " ORDER BY dist DESC;"
+						t.args.Reverse = true
+					} else {
+						t.args.Query += " ORDER BY dist ASC;"
+					}
+				} else {
+					t.args.Query += " ORDER BY dist ASC;"
+				}
+			} else if r.URL.Query()["order"] != nil {
+				if r.URL.Query()["order"][0] == "desc" {
+					t.args.Query += " ORDER BY words.funsort DESC;"
+					t.args.Reverse = true
+				} else {
+					t.args.Query += " ORDER BY words.funsort ASC;"
+				}
 			}
-		}
-		if r.URL.Query()["order"] != nil {
 			if r.URL.Query()["order"][0] == "desc" {
-				t.query += "ORDER BY subcipher DESC;"
+				t.args.CurrentPage += "&order=desc"
+			} else {
+				t.args.CurrentPage += "&order=asc"
 			}
+
 		} else {
-			t.query += ";"
-		}
-		if r.URL.Query()["start"] != nil {
-			t.start, err = strconv.Atoi(r.URL.Query()["start"][0])
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			t.start = 0
+			t.args.Query += ";"
 		}
 		if r.URL.Query()["num"] != nil {
-			t.num, err = strconv.Atoi(r.URL.Query()["num"][0])
+			currNum, err := strconv.Atoi(r.URL.Query()["num"][0])
 			if err != nil {
 				log.Fatal(err)
 			}
-		} else {
-			t.num = 10
+			t.args.Num = currNum
+			t.args.NextPage = t.args.CurrentPage + "&num=" + strconv.Itoa(currNum)
+
+			if r.URL.Query()["start"] != nil {
+				currStart, err := strconv.Atoi(r.URL.Query()["start"][0])
+				if err != nil {
+					log.Fatal(err)
+				}
+				t.args.Start = currStart
+				t.args.NextPage += "&start=" + strconv.Itoa(currStart+currNum)
+				if currStart >= currNum {
+					// PreviousPage will be nil on first page of results
+					t.args.PreviousPage = t.args.CurrentPage + "&num=" + strconv.Itoa(currNum)
+					t.args.PreviousPage += "&start=" + strconv.Itoa(currStart-currNum)
+				} else {
+					t.args.PreviousPage = ""
+				}
+
+				if err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				t.args.Start = 0
+				t.args.NextPage += "&start=" + strconv.Itoa(0+DefaultNum)
+			}
+		} else { // if missing either num or start query string
+			t.args.Num = DefaultNum
+			t.args.Start = 0
+			t.args.NextPage = t.args.CurrentPage + "&num=" + strconv.Itoa(DefaultNum)
+			t.args.NextPage += "&start=" + strconv.Itoa(0+DefaultNum)
 		}
 
 		t.templ = template.Must(template.New(t.filename).Funcs(funcMap).ParseFiles(
@@ -89,7 +149,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		))
 	}
 
-	t.templ.Execute(w, dbconnect.PostgresIO(t.query, t.start, t.num))
+	// if numrows := dbconnect.CountRows(t.queryFrom); numrows <= t.args.Start {
+	// 	t.args.NextPage = ""
+	// }
+
+	t.templ.Execute(w, t.args)
 }
 
 // Sets HTTP headers for handler passed to function
@@ -102,11 +166,26 @@ func setHeaders(h http.Handler) http.Handler {
 	})
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "relative/path/to/favicon.ico")
+}
+
 func main() {
 	http.Handle("/static/", setHeaders(http.StripPrefix("/static/", http.FileServer(http.Dir("static")))))
-	http.Handle("/", setHeaders(&templateHandler{filename: "words.html", path: "/", query: "SELECT * FROM formatted"}))
-	http.Handle("/runestats", setHeaders(&templateHandler{filename: "runestats.html"}))
-	http.Handle("/levdist", setHeaders(&templateHandler{filename: "levdist.html"}))
+	http.Handle("/", &templateHandler{filename: "words.html", path: "/",
+		query: `SELECT words.fun,
+				words.funsil,
+				words.trud,
+				words.pus,
+				words.numsil,
+				words.dist,
+				words.funsort`,
+		queryFrom: ` FROM words
+					WHERE words.kamin = true`},
+	)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	http.Handle("/runestats", &templateHandler{filename: "runestats.html"})
+	http.Handle("/levdist", &templateHandler{filename: "levdist.html"})
 
 	// start server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- params/main.go
+++ params/main.go
@@ -2,8 +2,14 @@ package params
 
 // Params encapsulates data to be passed to mapped functions
 type Params struct {
-	Fun     bool
-	Trud    bool
-	Dist    bool
-	Reverse bool
+	New          bool
+	Old          bool
+	Dist         bool
+	Reverse      bool
+	Query        string
+	Start        int
+	Num          int
+	CurrentPage  string
+	NextPage     string
+	PreviousPage string
 }
