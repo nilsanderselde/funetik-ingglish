1aea3b9cb3b85b24cb956e1c220c03102017dcfe
nilsanderselde <nils.elde@gmail.com>
2017-10-08 15:45:41 -0400

    prevented bug from word consisting entirely of symbols, and also implemented adding unknown words (consisting only of non-symbol, non-digit, non-punctuation characters) to a table

--- dbconnect/translit.go
+++ dbconnect/translit.go
@@ -6,6 +6,7 @@ package dbconnect
 import (
 	"bufio"
 	"database/sql"
+	"fmt"
 	"net/http"
 	"strings"
 	"unicode"
@@ -56,20 +57,20 @@ func ProcessTrud(ch chan Output, r *http.Request) {
 					for _, trud := range newWords {
 						if trud != "" { // don't store empty strings as words
 							words = append(words, trud) // add truditional spelling to list of words
+							words = append(words, "\n")
 						}
 					}
 					// Add a newline character as a "word" to be reinserted before displaying results
-					words = append(words, "\n")
+
 				}
 
 				var output string
 				// transliterate words
 				for _, trud := range words {
 					// if it's a single symbol by itself, just add it to output
-					if len(trud) == 1 && (unicode.IsPunct([]rune(trud)[0]) || trud == "\n") {
+					if r := []rune(trud)[0]; len(trud) == 1 && (unicode.IsPunct(r) || unicode.IsSymbol(r) || r == '\n') {
 						output += trud
 						continue
-
 					}
 					output += getFun(trud) + " "
 				}
@@ -99,52 +100,25 @@ func getFun(trud string) (fun string) {
 		// log.Fatal(err)
 		return ""
 	}
-	stringR := []rune(trud)
+	trudR := []rune(trud)
 	var leading string
 	var trailing string
 
 	// as long as first character is punctuation, add it to leading symbol string
 	// and remove it from word string (trud)
-	for unicode.IsPunct(stringR[0]) {
-		leading += string(stringR[0])
-		stringR = stringR[1:]
-		// fmt.Printf("{%s},{%s}\n", leading, string(stringR))
-
+	for len(trudR) > 1 && (unicode.IsPunct(trudR[0]) || unicode.IsSymbol(trudR[0])) {
+		leading += string(trudR[0])
+		trudR = trudR[1:]
+		// fmt.Printf("{%s},{%s}\n", leading, string(trudR))
 	}
 	// as long as last character is punctuation, add it to trailing symbol string
 	// and remove it from word string (trud)
-	for unicode.IsPunct(stringR[len(stringR)-1]) {
-		trailing += string(stringR[len(stringR)-1])
-		stringR = stringR[0 : len(stringR)-1]
-		// fmt.Printf("{%s},{%s}\n", string(stringR), trailing)
+	for len(trudR) > 1 && (unicode.IsPunct(trudR[len(trudR)-1]) || unicode.IsSymbol(trudR[len(trudR)-1])) {
+		trailing += string(trudR[len(trudR)-1])
+		trudR = trudR[0 : len(trudR)-1]
+		// fmt.Printf("{%s},{%s}\n", string(trudR), trailing)
 	}
-	trud = replaceRunes(stringR) //
-
-	// if trud contains hyphen in middle
-	if strings.ContainsRune(trud, '-') {
-		// split trud by hyphen and recursively process each part
-		trudSplit := strings.Split(trud, "-")
-		// if len(trudSplit) > 1 {
-		newTrud := getFun(trudSplit[0])
-		for i := 1; i < len(trudSplit); i++ {
-			newTrud += "-" + getFun(trudSplit[i])
-		}
-		trud = newTrud
-		// }
-	}
-
-	// if trud contains slash in middle
-	if strings.ContainsRune(trud, '/') {
-		// split trud by slash and recursively process each part
-		trudSplit := strings.Split(trud, "/")
-		// if len(trudSplit) > 1 {
-		newTrud := getFun(trudSplit[0])
-		for i := 1; i < len(trudSplit); i++ {
-			newTrud += "/" + getFun(trudSplit[i])
-		}
-		trud = newTrud
-		// }
-	}
+	trud = replaceRunes(trudR) //
 
 	// determine if case is upper or title, optimisticly
 	isUpperCase := true
@@ -160,16 +134,63 @@ func getFun(trud string) (fun string) {
 
 	// update fun and/or numsil with values generated using funsil
 	// fmt.Println(">>", trud)
-	rows := db.QueryRow("SELECT COALESCE(ritin, fun) FROM words WHERE trud = $1;", trud)
-	err = rows.Scan(&fun)
+	row := db.QueryRow("SELECT COALESCE(ritin, fun) FROM words WHERE trud = $1;", trud)
+	err = row.Scan(&fun)
 	if err != nil {
 		// fmt.Println("not found, checking lowercase:", trud)
-		rows = db.QueryRow("SELECT COALESCE(ritin, fun) FROM words WHERE trud = $1;", strings.ToLower(trud))
-		err = rows.Scan(&fun)
+		row = db.QueryRow("SELECT COALESCE(ritin, fun) FROM words WHERE trud = $1;", strings.ToLower(trud))
+		err = row.Scan(&fun)
 		if err != nil {
-			// fmt.Println("not found, returning trud:", trud)
-			UnknownWords = append(UnknownWords, trud)
-			return leading + trud + trailing
+			// renamed variable as it may be either an unknown word returned in original form or a hyphenated/slashed word
+			// return as transliterated sum of its parts
+			word := trud
+
+			// if word contains hyphen or slash in middle, dont add to list of unknown words (as it is compound)
+			// if word contains hyphen in middle
+			if strings.ContainsRune(word, '-') {
+				// split word by hyphen and recursively process each part
+				wordSplit := strings.Split(word, "-")
+				newWord := getFun(wordSplit[0])
+				for i := 1; i < len(wordSplit); i++ {
+					newWord += "-" + getFun(wordSplit[i])
+				}
+				word = newWord
+			}
+
+			// if word contains slash in middle
+			if strings.ContainsRune(word, '/') {
+				// split word by slash and recursively process each part
+				wordSplit := strings.Split(word, "/")
+				newWord := getFun(wordSplit[0])
+				for i := 1; i < len(wordSplit); i++ {
+					newWord += "/" + getFun(wordSplit[i])
+				}
+				word = newWord
+			}
+
+			addToList := true
+			wordR := []rune(word)
+			for _, r := range wordR {
+				if unicode.IsPunct(r) ||
+					unicode.IsSymbol(r) ||
+					unicode.IsDigit(r) {
+					addToList = false
+					break
+				}
+			}
+
+			// add to unknown word table
+			if addToList {
+				_, err = db.Exec(`INSERT INTO unknown (trud)
+SELECT '` + word + `'
+WHERE NOT EXISTS (SELECT trud FROM unknown WHERE LOWER(trud) = '` + strings.ToLower(word) + `')
+AND NOT EXISTS (SELECT trud FROM words WHERE LOWER(trud) = '` + strings.ToLower(word) + `')`)
+				if err != nil {
+					fmt.Println("word not saved")
+				}
+			}
+
+			return leading + word + trailing
 		}
 	}
 
