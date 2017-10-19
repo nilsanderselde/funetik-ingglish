6045f440765e48d5e52cbe0354359e2f8756ddd9
nilsanderselde <nils.elde@gmail.com>
2017-10-19 19:19:11 -0400

    implemented simplistic caching to prevent flooding db with queries

--- dbconnect/getwords.go
+++ dbconnect/getwords.go
@@ -5,21 +5,35 @@ package dbconnect
 
 import (
 	"database/sql"
+	"fmt"
 	"strconv"
+	"time"
 
 	// postgres drivers
 	_ "github.com/lib/pq"
 	"gitlab.com/nilsanderselde/funetik-ingglish/global"
 )
 
+var ready = true
+var wordList = [][]string{{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}}
+
 // ShowWords calls the database io function and passes arguments to template
 func ShowWords(args global.TemplateParams) [][]string {
+	fmt.Println("HTTP request")
+	for !ready {
+		time.Sleep(1000 * time.Millisecond)
+		ready = true
+		fmt.Println("Too many requests. Here's the cached version.")
+		return wordList
+	}
 	return GetWords(args.PQuery, args.Start, args.Num)
 }
 
 // GetWords receives SQL requests and returns requested data from the database. This package will not be shared in the repository for security reasons.
 // returns true if more results can be found (to avoid next button to empty page)
 func GetWords(query string, start int, num int) [][]string {
+	ready = false
+	fmt.Println("Getting words")
 
 	results := [][]string{}
 	notfound := [][]string{{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}}
@@ -61,6 +75,8 @@ func GetWords(query string, start int, num int) [][]string {
 	}
 	// Only return results if they are all good, otherwise return array with one row of -1s
 	if len(results) != 0 {
+		ready = true
+		wordList = results
 		return results
 	}
 	return notfound
