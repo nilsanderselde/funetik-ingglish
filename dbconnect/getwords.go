13be52156698eb3058e8be7bed5dbf9de1860b2b
nilsanderselde <nils.elde@gmail.com>
2017-10-19 20:32:36 -0400

    removed timer from wordlist caching (it was pointless)

--- dbconnect/getwords.go
+++ dbconnect/getwords.go
@@ -6,22 +6,20 @@ package dbconnect
 import (
 	"database/sql"
 	"strconv"
-	"time"
 
 	// postgres drivers
 	_ "github.com/lib/pq"
 	"gitlab.com/nilsanderselde/funetik-ingglish/global"
 )
 
-var ready = true
-var wordList = [][]string{{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}}
+var busy bool
+var wordList [][]string
 
 // ShowWords calls the database io function and passes arguments to template
 func ShowWords(args global.TemplateParams) [][]string {
 	// fmt.Println("HTTP request")
-	for !ready {
-		time.Sleep(1000 * time.Millisecond)
-		ready = true
+	for busy {
+		busy = false
 		// fmt.Println("Too many requests. Here's the cached version.")
 		return wordList
 	}
@@ -31,22 +29,20 @@ func ShowWords(args global.TemplateParams) [][]string {
 // GetWords receives SQL requests and returns requested data from the database. This package will not be shared in the repository for security reasons.
 // returns true if more results can be found (to avoid next button to empty page)
 func GetWords(query string, start int, num int) [][]string {
-	ready = false
+	busy = true
 	// fmt.Println("Getting words")
 
 	results := [][]string{}
-	notfound := [][]string{{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}}
+	noresults := [][]string{{"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}}
 
 	// get query
 	rows, err := DB.Query(query)
 	if err != nil {
-		// log.Fatal(err)
-		return notfound
+		goto loadfailure
 	}
 	defer rows.Close()
 
-	i := 0
-	for rows.Next() && i < start+num {
+	for i := 0; rows.Next() && i < start+num; i++ {
 		if i >= start {
 			var id int
 			var fun string
@@ -61,24 +57,22 @@ func GetWords(query string, start int, num int) [][]string {
 			results = append(results, []string{strconv.Itoa(id), fun, funsil, trud, strconv.Itoa(dist),
 				pus, strconv.Itoa(numsil), strconv.FormatBool(fläg)})
 			if err != nil {
-				// log.Fatal(err)
-				return notfound
+				goto loadfailure
 			}
 		}
-		i++
 	}
 	err = rows.Err()
 	if err != nil {
-		// log.Fatal(err)
-		return notfound
+		goto loadfailure
 	}
 	// Only return results if they are all good, otherwise return array with one row of -1s
 	if len(results) != 0 {
-		ready = true
+		busy = false
 		wordList = results
 		return results
 	}
-	return notfound
+loadfailure:
+	return noresults
 }
 
 // CountRows counts the number of rows returned by a query that is passed to it
