dfac13b1ac54e915693e93add54d8a869981fb4b
nilsanderselde <nils.elde@gmail.com>
2017-10-20 05:22:04 -0400

    added audio files for each phoneme on keyboard and home pages

--- kbdlogic.go
+++ kbdlogic.go
@@ -5,53 +5,53 @@ import (
 )
 
 var (
-	defTilde = []string{"`", "~", "", ""}
-	def1     = []string{"1", "!", "", ""}
-	def2     = []string{"2", "@", "", ""}
-	def3     = []string{"3", "#", "", ""}
-	def4     = []string{"4", "$", "", ""}
-	def5     = []string{"5", "%", "", ""}
-	def6     = []string{"6", "^", "", ""}
-	def7     = []string{"7", "&", "", ""}
-	def8     = []string{"8", "*", "", ""}
-	def9     = []string{"9", "(", "", ""}
-	def0     = []string{"0", ")", "", ""}
-	defDash  = []string{"-", "_", "", ""}
-	defEq    = []string{"=", "+", "", ""}
-	defQ     = []string{"ä", "Ä", "", ""}
-	defW     = []string{"w", "W", "", wIPA}
-	defE     = []string{"e", "E", "", ɛIPA}
-	defR     = []string{"r", "R", "", rIPA}
-	defT     = []string{"t", "T", "", tIPA}
-	defY     = []string{"y", "Y", "", jIPA}
-	defU     = []string{"u", "U", "", ʌIPA}
-	defI     = []string{"i", "I", "", ɪIPA}
-	defO     = []string{"o", "O", "", oIPA}
-	defP     = []string{"p", "P", "", pIPA}
-	defLb    = []string{"[", "{", "", ""}
-	defRb    = []string{"]", "}", "", ""}
-	defBksl  = []string{"\\", "|", "", ""}
-	defA     = []string{"a", "A", "", ɑIPA}
-	defS     = []string{"s", "S", "", sIPA}
-	defD     = []string{"d", "D", "", dIPA}
-	defF     = []string{"f", "F", "", fIPA}
-	defG     = []string{"g", "G", "", ɡIPA}
-	defH     = []string{"h", "H", "", hIPA}
-	defJ     = []string{"ø", "Ø", "", dʒIPA}
-	defK     = []string{"k", "K", "", kIPA}
-	defL     = []string{"l", "L", "", lIPA}
-	defSemi  = []string{";", ":", "", ""}
-	defQt    = []string{"'", "\"", "", ""}
-	defZ     = []string{"z", "Z", "", zIPA}
-	defX     = []string{"x", "X", "", ""}
-	defC     = []string{"c", "C", "", tʃIPA}
-	defV     = []string{"v", "V", "", vIPA}
-	defB     = []string{"b", "B", "", bIPA}
-	defN     = []string{"n", "N", "", nIPA}
-	defM     = []string{"m", "M", "", mIPA}
-	defCom   = []string{",", "<", "", ""}
-	defPer   = []string{".", ">", "", ""}
-	defSl    = []string{"/", "?", "", ""}
+	defTilde = []string{"`", "~", "", "", ""}
+	def1     = []string{"1", "!", "", "", ""}
+	def2     = []string{"2", "@", "", "", ""}
+	def3     = []string{"3", "#", "", "", ""}
+	def4     = []string{"4", "$", "", "", ""}
+	def5     = []string{"5", "%", "", "", ""}
+	def6     = []string{"6", "^", "", "", ""}
+	def7     = []string{"7", "&", "", "", ""}
+	def8     = []string{"8", "*", "", "", ""}
+	def9     = []string{"9", "(", "", "", ""}
+	def0     = []string{"0", ")", "", "", ""}
+	defDash  = []string{"-", "_", "", "", ""}
+	defEq    = []string{"=", "+", "", "", ""}
+	defQ     = []string{"ä", "Ä", "", "", ""}
+	defW     = []string{"w", "W", "", wIPA, "w"}
+	defE     = []string{"e", "E", "", ɛIPA, "e"}
+	defR     = []string{"r", "R", "", rIPA, "r"}
+	defT     = []string{"t", "T", "", tIPA, "t"}
+	defY     = []string{"y", "Y", "", jIPA, "y"}
+	defU     = []string{"u", "U", "", ʌIPA, "u"}
+	defI     = []string{"i", "I", "", ɪIPA, "i"}
+	defO     = []string{"o", "O", "", oIPA, "o"}
+	defP     = []string{"p", "P", "", pIPA, "p"}
+	defLb    = []string{"[", "{", "", "", ""}
+	defRb    = []string{"]", "}", "", "", ""}
+	defBksl  = []string{"\\", "|", "", "", ""}
+	defA     = []string{"a", "A", "", ɑIPA, "a"}
+	defS     = []string{"s", "S", "", sIPA, "s"}
+	defD     = []string{"d", "D", "", dIPA, "d"}
+	defF     = []string{"f", "F", "", fIPA, "f"}
+	defG     = []string{"g", "G", "", ɡIPA, "g"}
+	defH     = []string{"h", "H", "", hIPA, "h"}
+	defJ     = []string{"j", "J", "", dʒIPA, "dzh"}
+	defK     = []string{"k", "K", "", kIPA, "k"}
+	defL     = []string{"l", "L", "", lIPA, "l"}
+	defSemi  = []string{";", ":", "", "", ""}
+	defQt    = []string{"'", "\"", "", "", ""}
+	defZ     = []string{"z", "Z", "", zIPA, "z"}
+	defX     = []string{"x", "X", "", "", ""}
+	defC     = []string{"c", "C", "", tʃIPA, "tsh"}
+	defV     = []string{"v", "V", "", vIPA, "v"}
+	defB     = []string{"b", "B", "", bIPA, "b"}
+	defN     = []string{"n", "N", "", nIPA, "n"}
+	defM     = []string{"m", "M", "", mIPA, "m"}
+	defCom   = []string{",", "<", "", "", ""}
+	defPer   = []string{".", ">", "", "", ""}
+	defSl    = []string{"/", "?", "", "", ""}
 )
 
 var (
@@ -128,7 +128,7 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"ä", "Ä", "final", æIPA},
+				{"ä", "Ä", "final", æIPA, "aa"},
 				defW,
 				defE,
 				defR,
@@ -149,16 +149,16 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defF,
 				defG,
 				defH,
-				{"ʃ", "Ʃ", "old", ʃIPA},
+				{"ʃ", "Ʃ", "old", ʃIPA, "sh"},
 				defK,
-				{"л", "Л", "old", lIPA},
+				{"л", "Л", "old", lIPA, "l"},
 				defSemi,
 				defQt,
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"ʒ", "Ʒ", "old", ʒIPA},
-				{"ï", "Ï", "old", aɪIPA},
+				{"ʒ", "Ʒ", "old", ʒIPA, "zh"},
+				{"ï", "Ï", "old", aɪIPA, "ai"},
 				defV,
 				defB,
 				defN,
@@ -185,7 +185,7 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"æ", "Æ", "old", æIPA},
+				{"æ", "Æ", "old", æIPA, "aa"},
 				defW,
 				defE,
 				defR,
@@ -195,9 +195,9 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defI,
 				defO,
 				defP,
-				{"'", "\"", "old", ""},
-				{"?", "[", "old", ""},
-				{"/", "]", "old", ""},
+				{"'", "\"", "old", "", ""},
+				{"?", "[", "old", "", ""},
+				{"/", "]", "old", "", ""},
 			}
 			t.args.Kbd[2] = [][]string{
 				defA,
@@ -206,44 +206,44 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defF,
 				defG,
 				defH,
-				{"ʃ", "Ʃ", "old", ʃIPA},
+				{"ʃ", "Ʃ", "old", ʃIPA, "sh"},
 				defK,
-				{"λ", "Λ", "old", lIPA},
-				{"ā", "Ā", "old", eɪIPA},
-				{".", ":", "old", ""},
+				{"λ", "Λ", "old", lIPA, "l"},
+				{"ā", "Ā", "old", eɪIPA, "ei"},
+				{".", ":", "old", "", ""},
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"ē", "Ē", "old", iIPA},
-				{"ī", "Ī", "old", aɪIPA},
+				{"ē", "Ē", "old", iIPA, "y"},
+				{"ī", "Ī", "old", aɪIPA, "ai"},
 				defV,
 				defB,
 				defN,
 				defM,
-				{"ū", "Ū", "old", uIPA},
-				{"ʒ", "Ʒ", "old", ʒIPA},
-				{",", ";", "old", ""},
+				{"ū", "Ū", "old", uIPA, "w"},
+				{"ʒ", "Ʒ", "old", ʒIPA, "zh"},
+				{",", ";", "old", "", ""},
 			}
 			return
 
 		case "3":
 			t.args.Kbd[0] = [][]string{
-				{"ð", "Ð", "old", ðIPA},
+				{"ð", "Ð", "old", ðIPA, "dh"},
 				def1,
-				{"2", "/", "old", ""},
+				{"2", "/", "old", "", ""},
 				def3,
 				def4,
 				def5,
-				{"6", ":", "old", ""},
-				{"7", "?", "old", ""},
+				{"6", ":", "old", "", ""},
+				{"7", "?", "old", "", ""},
 				def8,
 				def9,
 				def0,
 				defDash,
-				{"'", "\"", "old", ""},
+				{"'", "\"", "old", "", ""},
 			}
 			t.args.Kbd[1] = [][]string{
-				{"æ", "Æ", "old", æIPA},
+				{"æ", "Æ", "old", æIPA, "aa"},
 				defW,
 				defE,
 				defR,
@@ -253,9 +253,9 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defI,
 				defO,
 				defP,
-				{"ч", "Ч", "old", tʃIPA},
-				{"θ", "Θ", "old", θIPA},
-				{"=", "+", "old", ""},
+				{"ч", "Ч", "old", tʃIPA, "tsh"},
+				{"θ", "Θ", "old", θIPA, "th"},
+				{"=", "+", "old", "", ""},
 			}
 			t.args.Kbd[2] = [][]string{
 				defA,
@@ -264,23 +264,23 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defF,
 				defG,
 				defH,
-				{"j", "J", "", dʒIPA},
+				{"j", "J", "", dʒIPA, "dzh"},
 				defK,
-				{"л", "Л", "old", lIPA},
-				{"ā", "Ā", "old", eɪIPA},
-				{"ʒ", "Ʒ", "old", ʒIPA},
+				{"л", "Л", "old", lIPA, "l"},
+				{"ā", "Ā", "old", eɪIPA, "ei"},
+				{"ʒ", "Ʒ", "old", ʒIPA, "zh"},
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"ē", "Ē", "old", iIPA},
-				{"ī", "Ī", "old", aɪIPA},
+				{"ē", "Ē", "old", iIPA, "y"},
+				{"ī", "Ī", "old", aɪIPA, "ai"},
 				defV,
 				defB,
 				defN,
 				defM,
-				{"ū", "Ū", "old", uIPA},
-				{"ʃ", "Ʃ", "old", ʃIPA},
-				{".", ",", "old", ""},
+				{"ū", "Ū", "old", uIPA, "w"},
+				{"ʃ", "Ʃ", "old", ʃIPA, "sh"},
+				{".", ",", "old", "", ""},
 			}
 			return
 		case "4":
@@ -300,14 +300,14 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"ä", "Ä", "final", æIPA},
+				{"ä", "Ä", "final", æIPA, "aa"},
 				defW,
 				defE,
 				defR,
 				defT,
 				defY,
 				defU,
-				defI,
+				{"i", "i", "", aɪIPA, "ai"},
 				defO,
 				defP,
 				defLb,
@@ -321,7 +321,7 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defF,
 				defG,
 				defH,
-				{"j", "J", "", dʒIPA},
+				{"j", "J", "", dʒIPA, "dzh"},
 				defK,
 				defL,
 				defSemi,
@@ -329,8 +329,8 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"ĭ", "Ĭ", "old", ɪIPA},
-				{"c", "C", "", tʃIPA},
+				{"ĭ", "Ĭ", "old", ɪIPA, "i"},
+				{"c", "C", "", tʃIPA, "tsh"},
 				defV,
 				defB,
 				defN,
@@ -357,12 +357,12 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"æ", "Æ", "old", æIPA},
-				{"ū", "Ū", "old", uwIPA},
+				{"æ", "Æ", "old", æIPA, "aa"},
+				{"ū", "Ū", "old", uwIPA, "w"},
 				defE,
 				defR,
 				defT,
-				{"ē", "Ē", "old", ijIPA},
+				{"ē", "Ē", "old", ijIPA, "y"},
 				defU,
 				defI,
 				defO,
@@ -378,16 +378,16 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defF,
 				defG,
 				defH,
-				{"ā", "Ā", "old", eɪIPA},
+				{"ā", "Ā", "old", eɪIPA, "ei"},
 				defK,
-				{"л", "Л", "old", lIPA},
+				{"л", "Л", "old", lIPA, "l"},
 				defSemi,
 				defQt,
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"ʊ", "Ʊ", "old", ʊIPA},
-				{"ī", "Ī", "old", aɪIPA},
+				{"ʊ", "Ʊ", "old", ʊIPA, "oo"},
+				{"ī", "Ī", "old", aɪIPA, "ai"},
 				defV,
 				defB,
 				defN,
@@ -414,37 +414,37 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"ō", "Ō", "old", oIPA},
-				{"ū", "Ū", "old", uwIPA},
+				{"ō", "Ō", "old", oIPA, "o"},
+				{"ū", "Ū", "old", uwIPA, "w"},
 				defE,
 				defR,
 				defT,
-				{"ē", "Ē", "old", ijIPA},
+				{"ē", "Ē", "old", ijIPA, "y"},
 				defU,
 				defI,
-				defO,
+				{"o", "O", "", ɑIPA, "a"},
 				defP,
 				defLb,
 				defRb,
 				defBksl,
 			}
 			t.args.Kbd[2] = [][]string{
-				defA,
+				{"a", "A", "", æIPA, "aa"},
 				defS,
 				defD,
 				defF,
 				defG,
 				defH,
-				{"ā", "Ā", "old", eɪIPA},
+				{"ā", "Ā", "old", eɪIPA, "ei"},
 				defK,
-				{"л", "Л", "old", lIPA},
+				{"л", "Л", "old", lIPA, "l"},
 				defSemi,
 				defQt,
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"ø", "Ø", "old", ʊIPA},
-				{"ī", "Ī", "old", aɪIPA},
+				{"ø", "Ø", "old", ʊIPA, "oo"},
+				{"ī", "Ī", "old", aɪIPA, "ai"},
 				defV,
 				defB,
 				defN,
@@ -456,7 +456,7 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 			return
 		case "7":
 			t.args.Kbd[0] = [][]string{
-				{"ž", "Ž", "old", ʒIPA},
+				{"ž", "Ž", "old", ʒIPA, "zh"},
 				def1,
 				def2,
 				def3,
@@ -471,22 +471,22 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"ó", "Ó", "old", oIPA},
-				{"ú", "Ú", "old", uwIPA},
+				{"ó", "Ó", "old", oIPA, "o"},
+				{"ú", "Ú", "old", uwIPA, "w"},
 				defE,
 				defR,
 				defT,
-				{"é", "É", "old", ijIPA},
+				{"é", "É", "old", ijIPA, "y"},
 				defU,
 				defI,
-				defO,
+				{"o", "O", "", ɑIPA, "a"},
 				defP,
-				{"ø", "Ø", "old", ʊIPA},
-				{"š", "Š", "old", ʃIPA},
+				{"ø", "Ø", "old", ʊIPA, "oo"},
+				{"š", "Š", "old", ʃIPA, "sh"},
 				defBksl,
 			}
 			t.args.Kbd[2] = [][]string{
-				defA,
+				{"a", "A", "", æIPA, "aa"},
 				defS,
 				defD,
 				defF,
@@ -495,19 +495,19 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defJ,
 				defK,
 				defL,
-				{"á", "Á", "old", eɪIPA},
+				{"á", "Á", "old", eɪIPA, "ei"},
 				defQt,
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"í", "Í", "old", aɪIPA},
+				{"í", "Í", "old", aɪIPA, "ai"},
 				defC,
 				defV,
 				defB,
 				defN,
 				defM,
-				{",", ";", "old", ""},
-				{".", ":", "old", ""},
+				{",", ";", "old", "", ""},
+				{".", ":", "old", "", ""},
 				defSl,
 			}
 			return
@@ -528,28 +528,28 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 				defEq,
 			}
 			t.args.Kbd[1] = [][]string{
-				{"ó", "Ó", "old", oIPA},
-				{"ú", "Ú", "old", uwIPA},
+				{"ó", "Ó", "old", oIPA, "o"},
+				{"ú", "Ú", "old", uwIPA, "w"},
 				defE,
 				defR,
 				defT,
-				{"é", "É", "old", ijIPA},
+				{"é", "É", "old", ijIPA, "y"},
 				defU,
 				defI,
-				defO,
+				{"o", "O", "", ɑIPA, "a"},
 				defP,
 				defLb,
 				defRb,
 				defBksl,
 			}
 			t.args.Kbd[2] = [][]string{
-				defA,
+				{"a", "A", "", æIPA, "aa"},
 				defS,
 				defD,
 				defF,
 				defG,
 				defH,
-				{"ø", "Ø", "final", ʊIPA},
+				{"ø", "Ø", "final", ʊIPA, "oo"},
 				defK,
 				defL,
 				defSemi,
@@ -557,8 +557,8 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 			}
 			t.args.Kbd[3] = [][]string{
 				defZ,
-				{"í", "Í", "old", aɪIPA},
-				{"á", "Á", "old", eɪIPA},
+				{"í", "Í", "old", aɪIPA, "ai"},
+				{"á", "Á", "old", eɪIPA, "ei"},
 				defV,
 				defB,
 				defN,
@@ -593,12 +593,12 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 			defEq,
 		}
 		t.args.Kbd[1] = [][]string{
-			{"ä", "Ä", "final", æIPA},
-			{"w", "W", "", uwIPA},
+			{"ä", "Ä", "final", æIPA, "aa"},
+			{"w", "W", "", uwIPA, "w"},
 			defE,
 			defR,
 			defT,
-			{"y", "Y", "", ijIPA},
+			{"y", "Y", "", ijIPA, "y"},
 			defU,
 			defI,
 			defO,
@@ -614,7 +614,7 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 			defF,
 			defG,
 			defH,
-			{"ø", "Ø", "final", ʊIPA},
+			{"ø", "Ø", "final", ʊIPA, "oo"},
 			defK,
 			defL,
 			defSemi,
@@ -622,8 +622,8 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 		}
 		t.args.Kbd[3] = [][]string{
 			defZ,
-			{"ž", "Ž", "final", ʒIPA},
-			{"š", "Š", "final", ʃIPA},
+			{"ž", "Ž", "final", ʒIPA, "zh"},
+			{"š", "Š", "final", ʃIPA, "sh"},
 			defV,
 			defB,
 			defN,
@@ -633,5 +633,4 @@ func pickKeyboard(t *templateHandler, r *http.Request) {
 			defSl,
 		}
 	}
-
 }
